f1d29a814264bd9cf3426a5ff1b690c79fe791c3
Stefan Staub <sstaub@users.noreply.github.com>
2018-08-29 12:45:55 +0200

    Update WIFI_NTP.ino

--- examples/WIFI_NTP/WIFI_NTP.ino
+++ examples/WIFI_NTP/WIFI_NTP.ino
@@ -5,8 +5,8 @@
 #include "WiFiUdp.h"
 #include "NTP.h"
 
-char *ssid     = "yourSSID";
-char *password = "yourPASSWORD";
+char ssid[]     = "yourSSID";
+char password[] = "yourPASSWORD";
 
 WiFiUDP wifiUdp;
 NTP ntp(wifiUdp);
